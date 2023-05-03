44bd518e28c071dfb1c1b37b6c997871b86776d5
zen-pc <zennomi.kun@gmail.com>
2023-05-03 13:17:02 +0700

    build 03/05

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a49b7e61562c2a89"; }
+/******/ 		__webpack_require__.h = function() { return "a56fb308fc4994ea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -198,7 +198,7 @@
 /******/ 					script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 				}
 /******/ 				script.setAttribute("data-webpack", dataWebpackPrefix + key);
-/******/ 				script.src = url;
+/******/ 				script.src = __webpack_require__.tu(url);
 /******/ 			}
 /******/ 			inProgress[url] = [done];
 /******/ 			var onScriptComplete = function(prev, event) {
@@ -242,6 +242,34 @@
 /******/ 	/* webpack/runtime/runtimeId */
 /******/ 	!function() {
 /******/ 		__webpack_require__.j = "webpack";
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/trusted types policy */
+/******/ 	!function() {
+/******/ 		var policy;
+/******/ 		__webpack_require__.tt = function() {
+/******/ 			// Create Trusted Type policy if Trusted Types are available and the policy doesn't exist yet.
+/******/ 			if (policy === undefined) {
+/******/ 				policy = {
+/******/ 					createScript: function(script) { return script; },
+/******/ 					createScriptURL: function(url) { return url; }
+/******/ 				};
+/******/ 				if (typeof trustedTypes !== "undefined" && trustedTypes.createPolicy) {
+/******/ 					policy = trustedTypes.createPolicy("nextjs#bundler", policy);
+/******/ 				}
+/******/ 			}
+/******/ 			return policy;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/trusted types script */
+/******/ 	!function() {
+/******/ 		__webpack_require__.ts = function(script) { return __webpack_require__.tt().createScript(script); };
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/trusted types script url */
+/******/ 	!function() {
+/******/ 		__webpack_require__.tu = function(url) { return __webpack_require__.tt().createScriptURL(url); };
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/hot module replacement */
@@ -1253,6 +1281,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     PRODUCTION_API: 'https://zenno.moe/api',
     GOOGLE_API: '',
     CUUTROM_API: 'http://localhost:3000',
-    ROMCOM_API: 'http://localhost:3000',
+    ROMCOM_API: 'https://backend.zenno.moe',
   },
   images: {
     domains: ['flagcdn.com'],
